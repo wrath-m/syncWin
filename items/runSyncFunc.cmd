cd1834f521303d779b6da585d167f043992949c6
Scarlight <filip.toporan@gmail.com>
2017-09-11 21:20:23 +0300

    update

--- items/runSyncFunc.cmd
+++ items/runSyncFunc.cmd
@@ -1,2 +1,4 @@
+cp "%USERPROFILE\syncCore\items\syncRunProcess.cmd" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\syncRunProcess.cmd"
 cd %USERPROFILE%\syncCore
 ssh-agent bash -c "ssh-add '%USERPROFILE%\syncCore\ssh-keys\id_rsa'; git pull"
+:: copy all watched files
